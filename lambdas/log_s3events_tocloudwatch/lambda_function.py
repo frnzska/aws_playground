d4478b8c8b9a036c9b6c5b5d78a6ea18578564a5
frnzska <fadler@babbel.com>
2017-07-18 08:55:45 +0200

    add s3 event logging lambda function

---
+++ lambdas/log_s3events_tocloudwatch/lambda_function.py
@@ -0,0 +1,24 @@
+import logging
+import cfnresponse
+import os
+import boto3
+import json
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.debug('Loading function')
+
+client = boto3.client('cloudformation')
+
+
+def lambda_handler(event, context):
+    logger.info('LogS3DataEvents')
+    logger.info(json.dumps(event))
+
+    key, val = 'LogicalResourceId', 'functionInvocation'
+    if key in event and event[key] == val:
+        response = client.describe_stacks(StackName=os.environ['STACK_NAME'])
+        response_info = {}
+        response_info['Data'] = response['Stacks'][0]['StackId']
+        cfnresponse.send(event, context, cfnresponse.SUCCESS, response_info)
+    return event
