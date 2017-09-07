848aaacf684771f99845f6cba38af777b244097b
frnzska <fadler@babbel.com>
2017-09-07 12:12:37 +0200

    add params and sg to deeplearning stack

--- cloudformation/ec2/deepLearning_ec2.py
+++ cloudformation/ec2/deepLearning_ec2.py
@@ -48,3 +48,91 @@ s3_doc = Policy(
         )
     ]
 )
+
+s3_access_policy = iam.Policy(
+    "s3AccessPolicy",
+    PolicyName= sep.join([cfg['deepLearning_ec2']['EC2_STACK_NAME'], 's3ReadPolicy']),
+    PolicyDocument=s3_doc
+)
+
+
+s3_access_role = t.add_resource(iam.Role(
+    "S3AccessRole",
+    AssumeRolePolicyDocument={
+        'Statement': [{
+            'Effect': 'Allow',
+            'Principal': {
+                'Service': [
+                    'ec2.amazonaws.com'
+                ]
+            },
+            'Action': ['sts:AssumeRole']
+        }]
+    },
+    Policies=[s3_access_policy]
+))
+
+# Output
+t.add_output([
+    Output('S3AccessRole',
+           Value=Ref(s3_access_role),
+           Description='EC2 instance profile role'
+           )
+    ]
+)
+
+# --- instance ---
+
+t.add_mapping(
+    'AZMap', {
+        'eu-west-1a': {'SubnetId': SUBNET_WEST_1A},
+        'eu-west-1b': {'SubnetId': SUBNET_WEST_1B},
+        'eu-west-1c': {'SubnetId': SUBNET_WEST_1C},
+    })
+
+az_param = t.add_parameter(
+    Parameter(
+        'AZName',
+        Type='String',
+        Description='AvailabilityZone of the stack',
+        AllowedValues=['eu-west-1a', 'eu-west-1b', 'eu-west-1c']
+    ))
+
+stage_param = t.add_parameter(
+    Parameter(
+        'Stage',
+        Type='String',
+        Description='Stage of this stack: staging or production',
+        AllowedValues=['staging', 'production']
+    ))
+
+instance_type_param = t.add_parameter(
+    Parameter(
+        'InstanceType',
+        Type='String',
+        Description='Instance Type'
+    ))
+
+
+key_name_param = t.add_parameter(
+    Parameter(
+        'KeyName',
+        Type='String',
+        Description='Key Name',
+    ))
+
+
+SecurityGroup = t.add_resource(
+    ec2.SecurityGroup(
+        'simpleSg',
+        GroupDescription='Simple Security Group: Enable SSH access via port 22',
+        SecurityGroupIngress=[
+            {
+                'IpProtocol': 'tcp',
+                'CidrIp': '0.0.0.0/0',
+                'FromPort': '22',
+                'ToPort': '22'
+            }
+        ]
+    )
+)
