782b5756bc5d0c9b76357a956cbafe2ce74095c2
frnzska <fadler@babbel.com>
2017-09-26 11:42:01 +0200

    change wrong path

--- cloudformation/ec2/scripts/jupyter_notebook_config.py
+++ cloudformation/ec2/scripts/jupyter_notebook_config.py
@@ -2,9 +2,9 @@ import os
 c = get_config()
 os.environ['LD_LIBRARY_PATH'] = '/usr/local/cuda-7.5/lib64'
 c.Spawner.env.update('LD_LIBRARY_PATH')
-c.NotebookApp.certfile = '/home/ubuntu/ssl/cert.pem'
-c.NotebookApp.keyfile = '/home/ubuntu/ssl/cert.key'
+c.NotebookApp.certfile = '/home/ec2-user/ssl/cert.pem'
+c.NotebookApp.keyfile = '/home/ec2-user/ssl/cert.key'
 c.NotebookApp.ip = '*'
 c.NotebookApp.open_browser = False
 c.NotebookApp.password = 'sha1:914bdd8e4ac9:1880dccd7e1a3a4d90fd1c41ae86dd56efc5926e'
-c.NotebookApp.notebook_dir = '/home/ec2-user/jupyter-notebooks'
+c.NotebookApp.notebook_dir = '/mnt/jupyter-notebooks'
