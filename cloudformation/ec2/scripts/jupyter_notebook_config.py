0dad44ff2f44124a482400b518657d7888c41234
frnzska <fadler@babbel.com>
2017-09-26 11:25:17 +0200

    add x509 certificate to notebook config

--- cloudformation/ec2/scripts/jupyter_notebook_config.py
+++ cloudformation/ec2/scripts/jupyter_notebook_config.py
@@ -2,6 +2,8 @@ import os
 c = get_config()
 os.environ['LD_LIBRARY_PATH'] = '/usr/local/cuda-7.5/lib64'
 c.Spawner.env.update('LD_LIBRARY_PATH')
+c.NotebookApp.certfile = '/home/ubuntu/ssl/cert.pem'
+c.NotebookApp.keyfile = '/home/ubuntu/ssl/cert.key'
 c.NotebookApp.ip = '*'
 c.NotebookApp.open_browser = False
 c.NotebookApp.password = 'sha1:914bdd8e4ac9:1880dccd7e1a3a4d90fd1c41ae86dd56efc5926e'
