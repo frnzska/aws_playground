7c42bad57be52509cc196a5c167cdbc14ef137ac
frnzska <fadler@babbel.com>
2017-10-02 11:41:30 +0200

    rm copy paste error

--- cloudformation/ec2/scripts/certificate_and_s3fuse.sh
+++ cloudformation/ec2/scripts/certificate_and_s3fuse.sh
@@ -16,8 +16,8 @@ cd s3fs-fuse/
 ./autogen.sh
 ./configure
 make
-sudo make install
+make install
 echo user_allow_other >> /etc/fuse.conf
 mkdir -p /mnt/s3fs-cache
 
-/usr/local/bin/s3fs -o allow_other -o iam_role=auto -o umask=0 -o url=https://s3.amazonaws.com  -o no_check_certificate -o nonempty -o enable_noobj_cache -o use_cache=/mnt/s3fs-cache franziska-adler-general /mnt/jupyter-notebooks#!/usr/bin/env bash
+/usr/local/bin/s3fs -o allow_other -o iam_role=auto -o umask=0 -o url=https://s3.amazonaws.com  -o no_check_certificate -o nonempty -o enable_noobj_cache -o use_cache=/mnt/s3fs-cache franziska-adler-general /mnt/jupyter-notebooks
