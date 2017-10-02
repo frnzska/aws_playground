a618cdfd4692efd20f90b2e49745ef8be2446d87
frnzska <fadler@babbel.com>
2017-10-02 11:07:51 +0200

    change bucket in s3fuse script

--- cloudformation/ec2/scripts/certificate_and_s3fuse.sh
+++ cloudformation/ec2/scripts/certificate_and_s3fuse.sh
@@ -20,4 +20,4 @@ sudo make install
 echo user_allow_other >> /etc/fuse.conf
 mkdir -p /mnt/s3fs-cache
 
-/usr/local/bin/s3fs -o allow_other -o iam_role=auto -o umask=0 -o url=https://s3.amazonaws.com  -o no_check_certificate -o nonempty -o enable_noobj_cache -o use_cache=/mnt/s3fs-cache babbeldataeng-fadler-test /mnt/jupyter-notebooks#!/usr/bin/env bash
+/usr/local/bin/s3fs -o allow_other -o iam_role=auto -o umask=0 -o url=https://s3.amazonaws.com  -o no_check_certificate -o nonempty -o enable_noobj_cache -o use_cache=/mnt/s3fs-cache franziska-adler-general /mnt/jupyter-notebooks#!/usr/bin/env bash
