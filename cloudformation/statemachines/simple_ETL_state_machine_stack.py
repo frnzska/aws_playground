fe2a4a62aad2a89f6028bb302cffb13d2ad2f4fc
frnzska <fadler@babbel.com>
2017-11-05 19:44:05 +0100

    change config

--- cloudformation/statemachines/simple_ETL_state_machine_stack.py
+++ cloudformation/statemachines/simple_ETL_state_machine_stack.py
@@ -1,7 +1,6 @@
 from troposphere import Ref, iam, awslambda, GetAtt, Base64, Join, stepfunctions, Sub
 from troposphere import Template
 from awacs.aws import Statement, Action, Allow
-from troposphere.cloudformation import CustomResource
 import boto3
 import ruamel_yaml as yaml
 from pkg_resources import resource_string
@@ -13,11 +12,13 @@ import yaml
 cfg = yaml.load(resource_string('cloudformation', 'config.yml'))
 
 STACK_NAME = cfg['simple_ETL_state_machine']['STACK_NAME']
-FUNCTION_NAME_1 = cfg['simple_ETL_state_machine']['FCT_1']
+FUNCTION_NAME_VALIDATE = cfg['simple_ETL_state_machine']['FCT_VALIDATE']
 DEPLOYMENT_BUCKET = cfg['simple_ETL_state_machine']['DEPLOYMENT_BUCKET']
-S3_KEY_1 = cfg['simple_ETL_state_machine']['S3_KEY_1']
-FUNCTION_NAME_2 = cfg['simple_ETL_state_machine']['FCT_2']
-S3_KEY_2 = cfg['simple_ETL_state_machine']['S3_KEY_2']
+S3_KEY_VALIDATE = cfg['simple_ETL_state_machine']['S3_KEY_VALIDATE']
+FUNCTION_NAME_ENRICH = cfg['simple_ETL_state_machine']['FCT_ENRICH']
+S3_KEY_ENRICH = cfg['simple_ETL_state_machine']['S3_KEY_ENRICH']
+FUNCTION_NAME_STORE = cfg['simple_ETL_state_machine']['FCT_STORE']
+S3_KEY_STORE = cfg['simple_ETL_state_machine']['S3_KEY_STORE']
 
 template = Template()
 description = 'Template for simple ETL state machine'
@@ -42,6 +43,23 @@ lambda_execution_role = template.add_resource(iam.Role(
                             Action('logs', '*')
                         ],
                         Resource=["arn:aws:logs:*:*:*"]
+                    ),
+                ]
+            }),
+        iam.Policy(
+            PolicyName='GrantS3',
+            PolicyDocument={
+                "Version": "2012-10-17",
+                "Statement": [
+                    Statement(
+                        Sid='S3Access',
+                        Effect=Allow,
+                        Action=[
+                            Action('s3', 'Get*'),
+                            Action('s3', 'List*'),
+                            Action('s3', 'PutObject')
+                        ],
+                        Resource=["*"]
                     ),
                 ]
             }),
@@ -96,13 +114,13 @@ state_execution_role = template.add_resource(iam.Role(
 validate_fct = template.add_resource(
     awslambda.Function(
         'ValidateFct',
-        FunctionName=FUNCTION_NAME_1,
+        FunctionName=FUNCTION_NAME_VALIDATE,
         Description='Lambda 1 for Simple ETL Statemachine',
         Handler='validate.lambda_handler',
         Role=GetAtt('LambdaExecutionRole', 'Arn'),
         Code=awslambda.Code(
             S3Bucket=DEPLOYMENT_BUCKET,
-            S3Key=S3_KEY_1,
+            S3Key=S3_KEY_VALIDATE,
         ),
         Runtime='python3.6',
         Timeout='30',
@@ -113,13 +131,13 @@ validate_fct = template.add_resource(
 enrich_fct = template.add_resource(
     awslambda.Function(
         'EnrichFct',
-        FunctionName=FUNCTION_NAME_2,
+        FunctionName=FUNCTION_NAME_ENRICH,
         Description='Enrich fct for Simple ETL Statemachine',
         Handler='enrich.lambda_handler',
         Role=GetAtt('LambdaExecutionRole', 'Arn'),
         Code=awslambda.Code(
             S3Bucket=DEPLOYMENT_BUCKET,
-            S3Key=S3_KEY_2,
+            S3Key=S3_KEY_ENRICH,
         ),
         Runtime='python3.6',
         Timeout='30',
@@ -127,28 +145,68 @@ enrich_fct = template.add_resource(
     )
 )
 
+store_fct = template.add_resource(
+    awslambda.Function(
+        'StoreFct',
+        FunctionName=FUNCTION_NAME_STORE,
+        Description='Enrich fct for Simple ETL Statemachine',
+        Handler='store.lambda_handler',
+        Role=GetAtt('LambdaExecutionRole', 'Arn'),
+        Code=awslambda.Code(
+            S3Bucket=DEPLOYMENT_BUCKET,
+            S3Key=S3_KEY_STORE,
+        ),
+        Runtime='python3.6',
+        Timeout='30',
+        MemorySize=128,
+        Environment = awslambda.Environment('LambdaVars', Variables={'bucket': 'franziska-adler-test-bucket'})
+    )
+)
 
 definition_str = json.dumps({
-  "StartAt": "A",
+  "StartAt": "Validate",
   "States": {
       "Validate": {
           "Type": "Task",
-          "Resource": ':'.format(['arn:aws:lambda:eu-west-1:369667221252:function', FUNCTION_NAME_1]),
-          #"ResultPath": "$.",
-          "Next": "B"
+          "Resource": ':'.join(['arn:aws:lambda:eu-west-1:369667221252:function', FUNCTION_NAME_VALIDATE]),
+          "Catch": [{
+              "ErrorEquals": ["TypeError"],
+              "Next": "Done"
+          }],
+          "Next": "ChoiceState"
+        },
+     "ChoiceState":{
+        "Type": "Choice",
+            "Choices": [
+                {
+                    "Variable": "$.record.name",
+                    "StringEquals": "user:created",
+                    "Next": "Enrich"
+                },
+            ],
+           "Default": "Store"
       },
-    "Enrich": {
-      "Type": "Task",
-      "Resource": ':'.format(['arn:aws:lambda:eu-west-1:369667221252:function', FUNCTION_NAME_2]),
-      "End": True
-    }
+     "Enrich": {
+         "Type": "Task",
+         "Resource": ':'.join(['arn:aws:lambda:eu-west-1:369667221252:function', FUNCTION_NAME_ENRICH]),
+         "Next": "Store",
+     },
+     "Store":  {
+         "Type": "Task",
+         "Resource": ':'.join(['arn:aws:lambda:eu-west-1:369667221252:function', FUNCTION_NAME_STORE]),
+         "Next": "Done"
+     },
+     "Done": {
+            "Type": "Pass",
+            "End": True
+     }
   }
 })
 
 statemachine = template.add_resource(
     stepfunctions.StateMachine(
-        'ExampleStateMachine',
-        DependsOn= 'ExampleFct',
+        'SimpleETLStateMachine',
+        DependsOn= ['StoreFct', 'EnrichFct', 'ValidateFct'],
         DefinitionString = definition_str,
         RoleArn = GetAtt('StateExecutionRole', 'Arn')
     )
@@ -162,7 +220,7 @@ t_json = template.to_json(indent=4)
 stack_args = {
     'StackName': STACK_NAME,
     'TemplateBody': template.to_json(indent=4),
-    'Capabilities': ['CAPABILITY_IAM',],
+    'Capabilities': ['CAPABILITY_IAM'],
 }
 
 cfn = boto3.client('cloudformation')
