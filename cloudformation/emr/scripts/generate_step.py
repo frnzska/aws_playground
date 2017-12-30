6655bf72d6e8a944af530e7fc47a3a8242fc25a8
frnzska <fadler@babbel.com>
2017-12-30 22:22:26 +0100

    add crappy step generation file

---
+++ cloudformation/emr/scripts/generate_step.py
@@ -0,0 +1,26 @@
+import boto3
+import os
+os.environ["AWS_DEFAULT_REGION"] = "eu-west-1"
+emr = boto3.client('emr')
+
+step_definition = {'ActionOnFailure': 'CONTINUE',
+                   'HadoopJarStep':
+                        {'Args':
+                            ['spark-submit',
+                             '--deploy-mode', 'client',
+                             '--master', 'yarn',
+                             '/home/hadoop/some_spark_task.py',
+                             ],
+                   'Jar': 'command-runner.jar'},
+                   'Name': 'test steps'}
+
+
+def get_cluster_id(name='ClusterWithSparkAndSteps'):
+    clusters = emr.list_clusters()['Clusters']
+    cluster_id = [c['Id'] for c in clusters if c['Name']==name][0]
+    return cluster_id
+
+id = get_cluster_id()
+print(id)
+response = emr.add_job_flow_steps(JobFlowId=id, Steps=[step_definition])
+print(response)
