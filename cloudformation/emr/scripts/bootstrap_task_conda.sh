39c21b355db8ee10e2b587c311d66e85b30e9a2b
frnzska <fadler@babbel.com>
2017-12-30 20:51:40 +0100

    change conda file name

--- cloudformation/emr/scripts/bootstrap_task_conda.sh
+++ cloudformation/emr/scripts/bootstrap_task_conda.sh
@@ -7,7 +7,6 @@ TASK=${1:-"s3://franziska-adler-deployments/production/aws_playground/cloudforma
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/conda
 
-
 echo '\nexport PATH=$HOME/conda/bin:$PATH' >> $HOME/.bashrc && source $HOME/.bashrc
 
 conda config --set always_yes yes --set changeps1 no
@@ -15,7 +14,7 @@ conda config -f --add channels conda-forge
 conda config -f --add channels defaults
 conda install toolz boto3
 
-rm ~/miniconda.sh
+rm Miniconda3-latest-Linux-x86_64.sh
 
 IS_MASTER=false
 if grep isMaster /mnt/var/lib/info/instance.json | grep true;
