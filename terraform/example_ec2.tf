34283303caff11ee1843fa3c0b3af98ae9666161
frnzska <fadler@babbel.com>
2018-01-15 10:39:37 +0100

    allow ssh to ec2

--- terraform/example_ec2.tf
+++ terraform/example_ec2.tf
@@ -4,10 +4,37 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-e5083683"
-  instance_type = "t2.micro"
+  ami             = "ami-e5083683"
+  instance_type   = "t2.micro"
+  key_name        = "franzi"
+  security_groups = ["${aws_security_group.allow_ssh.name}"]
 
   tags {
     Name = "my_instance_name"
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = "~/.ssh/franzi.pem"
+    timeout     = "1m"
+    agent       = true
+  }
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_all"
+  description = "Allow inbound SSH traffic from my everywhere"
+  vpc_id      = "vpc-ced904a9"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "Allow SSH"
   }
 }
