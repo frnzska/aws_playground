432dbf8eeb94a8a492ed8dbd5c4b88b447cdb0a6
frnzska <fadler@babbel.com>
2018-03-27 23:02:40 +0200

    rm security blogs from config

--- terraform/ec2_with_s3_access/config.tf
+++ terraform/ec2_with_s3_access/config.tf
@@ -1,20 +1,3 @@
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_all"
-  description = "Allow inbound SSH traffic from my everywhere"
-  vpc_id      = "vpc-ced904a9"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags {
-    Name = "Allow SSH"
-  }
-}
-
 variable "aws_region" {
   default = "eu-west-1"
 }
